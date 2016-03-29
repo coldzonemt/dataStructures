bd38407537abdefcde6260297b936701ea91a521
Kristin <coldzonemt@yahoo.com>
2016-03-28 22:04:32 -0700

    implement pre, post, inOrder BST traversals

--- spec/binaryTreeTests.js
+++ spec/binaryTreeTests.js
@@ -1,11 +1,60 @@
+
 (function () {
   'use strict';
 
-  describe('Binary Search Trees have many methods', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-        expect(true).toBe(true);
+  describe('Binary Trees have many methods', function () {
+
+    describe('it should add children to the tree', function () {
+        var tree = new BinaryTree(4); 
+        tree.setLeft(2); 
+        tree.setRight(8);
+        tree.left.setRight(3); 
+        tree.left.setLeft(1); 
+        tree.right.setLeft(5)
+        tree.right.setRight(6); 
+
+      it('should add 0 to the left child of the root', function () {
+         tree.left.left.setLeft(0);
+         expect(tree.left.left.left.value).toBe(0);
       });
+
+      it('should add 15 to the right child of the root', function () {
+         tree.right.right.setRight(15); 
+         expect(tree.right.right.right.value).toBe(15); 
+      });
+
     });
+
+    describe('it should count all the nodes on the tree', function () {
+      var tree = new BinaryTree(4); 
+      tree.setLeft(1); 
+      tree.setRight(5);
+
+      it('should return the number of nodes on the tree', function () {
+        var nodes = tree.countNodes(); 
+        expect(nodes).toBe(3);
+      });
+
+    });
+
+    describe('it should have search methods', function () {
+      var tree = new BinaryTree(5); 
+      tree.setRight(10); 
+      tree.setLeft(2); 
+      tree.left.setRight(1); 
+      tree.left.setLeft(0); 
+      tree.right.setLeft(12); 
+      tree.right.setRight(15); 
+
+      it('should have a depth-first search method', function () {
+        var treeVal = tree.DFSearch(16); 
+        expect(treeVal).toBe(false); 
+      });
+
+
+    });
+
+     
+
   });
 })();
--- src/trees/binaryTree.js
+++ src/trees/binaryTree.js
@@ -10,40 +10,83 @@ var BinaryTree = function(val){
 }
 
 BinaryTree.prototype.setLeft = function(val) {
-	this.left = val; 
+	this.left = new BinaryTree(val); 
 }
 
 BinaryTree.prototype.setRight = function(val) {
-	this.right = val; 
+	this.right = new BinaryTree(val); 
 }
 
-BinaryTree.prototype.BFSearch = function(val) {
-
+BinaryTree.prototype.getLevel = function(node) {
+	if(this.left === node || this.right === node) {
+		return 1; 
+	}
+	return; 
+	//return this.getLevel(node.right);
 }
 
 BinaryTree.prototype.DFSearch = function(val) {
+	if(this.left === null && this.right === null) {
+		return false; 
+	}
+	console.log(this.value);
+	if(this.value === val) {
+		return true; 
+	} 
+
+	if(this.left) {
+		this.left.DFSearch(val); 
+	}
+	if(this.right) {
+		this.right.DFSearch(val);
+	}
+}
+
+BinaryTree.prototype.BFSearch = function(val) {
 	
 }
 
-BinaryTree.prototype.countNodes = function(root) {
-	if(root.left === null && root.right === null) {
+BinaryTree.prototype.countNodes = function() {
+	if(this.left === null && this.right === null) {
 		return 1
 	}
-	return countNodes(root.left) + countNodes(root.right) + 1; 
+	return this.left.countNodes() + this.right.countNodes() + 1; 
+}
+
+BinaryTree.prototype.inOrder = function() {
+	if(this.left) {
+		this.left.inOrder(); 
+	}
+	console.log(this.value);
+	if(this.right) {
+		this.right.inOrder(); 
+	} 
+}
+
+BinaryTree.prototype.postOrder = function() {
+	if(this.left) {
+		this.left.inOrder(); 
+	}
+	if(this.right) {
+		this.right.inOrder(); 
+	}
+	console.log(this.value);
+}
+
+BinaryTree.prototype.preOrder = function() {
+	console.log(this.value); 
+	if(this.left) {
+		this.left.preOrder()
+	}
+	if(this.right) {
+		this.right.preOrder(); 
+	}
+}
+
+BinaryTree.prototype.leastCommonAncestor = function(node1, node2){
+
 }
 
 BinaryTree.prototype.subTrees = function(node, value) {
-
-}
-
-BinaryTree.prototype.inOrder = function(root) {
-
-}
-
-BinaryTree.prototype.postOrder = function(root) {
-
-}
-
-BinaryTree.prototype.preOrder = function(root) {
 
 }
